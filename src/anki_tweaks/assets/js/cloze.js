6b829cc5a869acfc42e7fcd772b357e869aa6558
Denis Volok <denis.volok@pm.me>
2024-05-05 21:28:53 +0000

    feat: add tweaks

---
+++ src/anki_tweaks/assets/js/cloze.js
@@ -0,0 +1,43 @@
+/**
+  * Script with logic related to cloze notes.
+  */
+
+window.anki_tweaks.showCloze = ({ isShowAll } = {}) => {
+  let clozes = document.querySelectorAll(`.cloze[data-cloze][data-ordinal="1"]:not(.cloze_revealed)`);
+
+  if (clozes.length === 0) {
+//    pycmd("anki_tweaks:cloze:no_more_hidden"); @TODO: perhaps need somehow disable event listeners for non-cloze cards
+    return;
+  }
+
+  if (isShowAll !== true) {
+    clozes = [clozes[0]];
+  }
+
+  clozes.forEach(cloze => {
+    const answer = cloze.dataset.cloze;
+
+    cloze.textContent = answer;
+    cloze.classList.add("cloze_revealed");
+  });
+};
+
+window.anki_tweaks.handleClozeKeyDown = (event) => {
+  const nextBindingCode = `Key${window.anki_tweaks.next_shortcut.toUpperCase()}`;
+  const allBindingCode = `Key${window.anki_tweaks.all_shortcut.toUpperCase()}`;
+
+  const isMatchingNextShortcut = event.code === nextBindingCode
+  && ((event.shiftKey === false && window.anki_tweaks.next_shortcut.match(/[a-z]/)) || (event.shiftKey === true && window.anki_tweaks.next_shortcut.match(/[A-Z]/)))
+  const isMatchingAllShortcut = event.code === allBindingCode
+  && ((event.shiftKey === false && window.anki_tweaks.all_shortcut.match(/[a-z]/)) || (event.shiftKey === true && window.anki_tweaks.all_shortcut.match(/[A-Z]/)))
+//  console.log(event.code, event.shiftKey)
+
+  if (isMatchingNextShortcut) {
+    window.anki_tweaks.showCloze();
+  } else if (isMatchingAllShortcut) {
+    window.anki_tweaks.showCloze({ isShowAll: true });
+  }
+};
+
+document.removeEventListener("keydown", window.anki_tweaks.handleClozeKeyDown);
+document.addEventListener("keydown", window.anki_tweaks.handleClozeKeyDown);
